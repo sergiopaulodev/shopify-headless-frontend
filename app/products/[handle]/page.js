f567f2f8be92eed86037b53aa96ebe1c87ae52c0
Sergio Torres Fernández <sergiopaulodev@gmail.com>
2025-12-23 19:15:10 -0300

    refactor: simplify product route handler

--- app/products/[handle]/page.js
+++ app/products/[handle]/page.js
@@ -10,9 +10,4 @@ export default async function ProductPage({ params }) {
     }
 
     return <ProductDetail product={product} />;
-
-        // <main>
-        //     <h1>{product.title}</h1>
-        //     <p>{product.description}</p>
-        // </main>
 };
