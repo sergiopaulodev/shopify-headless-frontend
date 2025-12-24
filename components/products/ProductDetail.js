103496b3bfb1f9538dc33c989e54665a7e2aafc6
Sergio Torres Fernández <sergiopaulodev@gmail.com>
2025-12-24 00:20:54 -0300

    chore:fix product components extenisons (.js → .jsx

--- components/products/ProductDetail.js
+++
@@ -1,8 +0,0 @@
-export function ProductDetail({ product }) {
-    return (
-        <article>
-            <h1>{product.title}</h1>
-            {product.description && <p>{product.description}</p>}
-        </article>
-    );
-}
