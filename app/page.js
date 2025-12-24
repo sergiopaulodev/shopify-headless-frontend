b5febedbf19e5da003e26764685cbeb83e3fee36
Sergio Torres Fernández <sergiopaulodev@gmail.com>
2025-12-24 00:23:40 -0300

    chore: cleanup obsolete product list markup

--- app/page.js
+++ app/page.js
@@ -8,19 +8,6 @@ export default async function HomePage() {
         <main>
             <h1>Products</h1>
             <ProductList products={products}/>
-            {/* <ul>
-                {products.map((product) => (
-                    
-                        <li key={product.id}>
-                            <a href={`/products/${product.handle}`}>
-                            {product.title}
-                            </a>
-                        </li>
-
-                        ) 
-                    )
-                }
-            </ul> */}
         </main>
     );
 }
